9991a2461d0fabe0218e9337cf8c7de4686c8e66
Ngan Tran <trann0709@gmail.com>
2023-01-15 18:47:10 -0600

    updated index.css and imported normalize.css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import './App.css'
-
 function App() {
-  return <h1>hello world</h1>
+  return (
+    <>
+      <h1>hello world</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum, ipsa?
+      </p>
+    </>
+  )
 }
 
 export default App
